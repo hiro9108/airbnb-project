22d80db963dd238c8343c4bdbfa9ab00432e01c8
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-07-22 12:59:14 -0700

    feat: add graphql function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import "../styles/globals.css";
 import { Provider } from "react-redux";
+import { ApolloProvider } from "@apollo/client";
 import { store } from "../app/store";
+import { client } from "./api/apollo";
 
 import "font-awesome/css/font-awesome.min.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ApolloProvider>
   );
 }
 
