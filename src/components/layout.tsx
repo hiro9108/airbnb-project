d79c59231393b10d58559dcd053a03671c0b49a9
Miu <miyuuu0728@gmail.com>
2021-08-27 00:35:52 -0700

    meta tags

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -13,8 +13,16 @@ const Layout: React.FC<metaProps> = ({ description, title, children }) => {
   return (
     <div>
       <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#fff" />
         <meta name="description" content={description} />
         <meta name="og:title" content={title} />
+        <meta
+          name="keywords"
+          content="airbnb, clone, booking, trip, travel, hotel, house, activity, local"
+        />
+        <meta name="author" content="Hiroshi Egawa, Miyu Setoyama" />
         <title>{title}</title>
       </Head>
       <main>{children}</main>
